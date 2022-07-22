bc87286f6812cff701a82533f51dd3dfe7cc299d
Paulo Rogério Tavares <paulorogeriota@gmail.com>
2022-07-22 20:33:57 -0300

    Add files via upload

--- project_progress.sql
+++ project_progress.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13-Jun-2022 às 04:32
+-- Tempo de geração: 23-Jul-2022 às 01:31
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 7.4.29
 
@@ -49,6 +49,57 @@ INSERT INTO `progress` (`id`, `id_project_progress`, `item`, `concluded`, `activ
 (10, 2, 'cadastrar clientes', 0, 1, '', '2022-06-12 22:28:19', '0000-00-00 00:00:00'),
 (11, 2, 'cadastrar funcionarios', 1, 1, '', '2022-06-12 22:28:23', '2022-06-12 22:28:27');
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `project`
+--
+
+CREATE TABLE `project` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `project` varchar(100) NOT NULL,
+  `obs` text DEFAULT NULL,
+  `directory` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `project`
+--
+
+INSERT INTO `project` (`id`, `project`, `obs`, `directory`) VALUES
+(1, 'Projeto Site1', 'obs site 1', '../laravel-site1'),
+(2, 'Projeto Site2', 'site 2', '../laravel-site2');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura stand-in para vista `vproject_progress`
+-- (Veja abaixo para a view atual)
+--
+CREATE TABLE `vproject_progress` (
+`id` int(11)
+,`id_project_progress` int(11)
+,`item` varchar(50)
+,`concluded` tinyint(1)
+,`active` tinyint(1)
+,`obs` text
+,`date_reg` datetime
+,`date_update` datetime
+,`id_project` int(10) unsigned
+,`project` varchar(100)
+,`directory` text
+,`obs_project` text
+);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para vista `vproject_progress`
+--
+DROP TABLE IF EXISTS `vproject_progress`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vproject_progress`  AS SELECT `prg`.`id` AS `id`, `prg`.`id_project_progress` AS `id_project_progress`, `prg`.`item` AS `item`, `prg`.`concluded` AS `concluded`, `prg`.`active` AS `active`, `prg`.`obs` AS `obs`, `prg`.`date_reg` AS `date_reg`, `prg`.`date_update` AS `date_update`, `prj`.`id` AS `id_project`, `prj`.`project` AS `project`, `prj`.`directory` AS `directory`, `prj`.`obs` AS `obs_project` FROM (`progress` `prg` join `project` `prj`) WHERE `prg`.`id_project_progress` = `prj`.`id``id`  ;
+
 --
 -- Índices para tabelas despejadas
 --
@@ -60,6 +111,12 @@ ALTER TABLE `progress`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -68,6 +125,12 @@ ALTER TABLE `progress`
 --
 ALTER TABLE `progress`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de tabela `project`
+--
+ALTER TABLE `project`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
